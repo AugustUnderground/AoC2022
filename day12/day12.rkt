402c42d91ac9156c31e361ffbfd379fc2bd19b49
Yannick Uhlmann <augustunderground@protonmail.com>
2023-01-07 17:27:36 +0100

    day 12

--- day12/day12.rkt
+++ day12/day12.rkt
@@ -4,27 +4,32 @@
 (require relation/function)
 (require threading)
 
-(define (list->pair l) (cons (first l) (second l)))
-(define (nub l) (~> l list->set set->list))
+(define (nub l)        (~> l list->set set->list))
+(define (member? l e)  (~> e (member l) list?))
+(define (n-rows m)     (length m))
+(define (n-cols m)     (length (car m)))
+(define (mref m p)     (~> m (list-ref (car p)) (list-ref (cdr p))))
+(define (mset m p v)   (list-set m (car p)
+                                   (list-set (list-ref m (car p)) (cdr p) v)))
+(define (midx m v)     (apply append
+                              (filter-not empty?
+                                          (zip-with (λ (r cs)
+                                                       (map (partial cons r) cs))
+                                                    (range (n-rows m))
+                                                    (map (λ (r) (indexes-of r v))
+                                                         m)))))
 
-(define lines (file->lines "./day12ex.txt"))
+(define lines (file->lines "./day12.txt"))
 
-(define heights (map (λ (l) (~>> l string->list
-                                   (map char->integer)
-                                   (map (λ (c) (- c 96)))
-                                   (map (partial max 0))))
-                     lines))
+(define mtx (map (λ (l) (~>> l string->list
+                               (map char->integer)
+                               (map (λ (c) (- c 96)))
+                               (map (partial max 0))))
+                 lines))
 
-(define (n-rows m)   (length m))
-(define (n-cols m)   (length (car m)))
-(define (mref m p)   (~> m (list-ref (car p)) (list-ref (cdr p))))
-(define (mset m p v) (list-set m (car p) (list-set (list-ref m (car p)) (cdr p) v)))
-(define (midx m v)   (map list->pair (filter (λ (p) (all? p))
-                                             (zip (range (n-rows m))
-                                                  (map (λ (r) (index-of r v)) m)))))
+(define-values (s t) (apply values (midx mtx 0)))
 
-
-(define-values (s t) (apply values (midx heights 0)))
+(define heights (mset (mset mtx t 26) s 1))
 
 (define (cross coord)
   (let* ([r     (car coord)]
@@ -39,35 +44,28 @@
   (let* ([nr (n-rows m)]
          [nc (n-cols m)]
          [rv (mref m p)]
-         [l (λ (e) 
-               (let* ([y (car e)]
-                      [x (cdr e)]
-                      [v (mref m e)])
-                 (and (>= y 0) (>= x 0) (< y nr) (< x nc) (<= v (+ rv 1)))))])
+         [l (λ (e) (let* ([y (car e)] [x (cdr e)])
+                (and (>= y 0) (>= x 0) (< y nr) (< x nc)
+                     (<= (mref m e) (+ rv 1)))))])
     (~>> p cross (filter l))))
 
-(define (find-path grid path)
-    (let* ([next-points (mx grid (car path))])
-      (cond [(member t next-points) (cons t path)]
-            [(empty? next-points) '()]
-            [else (find-path )]
-        )
-      (find-path grid new-path (+ steps 1))))
+(define (count-steps grid wave idx [inv #f])
+  (let* ([front (filter (compose1 (partial = 0) (partial mref wave))
+                        (nub (apply append (map (partial mx grid)
+                                                (midx wave idx)))))]
+         [next-idx (+ 1 idx)]
+         [next-wave (foldl (λ (c w) (mset w c next-idx)) wave front)])
+    (cond [(member? front t) idx] [(empty? front) idx] 
+          [(and inv (any? (map (λ (f) (= 26 (mref grid f))) front))) idx]
+          [else (count-steps grid next-wave next-idx inv)])))
 
-(define p (cons 2 2))
-(member '(1 . 5) (cross t))
+(define init-wave (mset (repeat (n-rows heights)
+                                (repeat (n-cols heights) 0)) s 1))
+(define num-steps (count-steps heights init-wave 1))
 
-(define v (mref heights p))
+(define inv-heights  (map (λ (r) (map (partial - 27) r)) heights))
+(define inv-wave (mset (repeat (n-rows heights) (repeat (n-cols heights) 0)) t 1))
+(define inv-num-steps (count-steps inv-heights inv-wave 1 #t))
 
-(define foo
-  (cons p (mx heights p))
-)
-
-(filter (λ (f) (<= (mref heights f) (+ v 1)))  foo)
-
-(define bar (nub (foldl append '() (map (partial mx heights) foo))))
-
-
-
-
-;(fprintf (current-output-port) "Puzzle 2: ~a\n" monkey-business-long)
+(fprintf (current-output-port) "Puzzle 1: ~a\n" num-steps)
+(fprintf (current-output-port) "Puzzle 2: ~a\n" inv-num-steps)
